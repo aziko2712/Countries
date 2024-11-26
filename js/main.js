cfcc8a69f1fa012e55290931779668a0405ed9f3
aziko2712 <azamat.sharifov.93@inbox.ru>
2024-11-26 21:41:42 +0500

    Add JS local-storage settings

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ const elSiteThemeToggler = document.querySelector('.site-header-theme-toggler');
 elSiteThemeToggler.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
 })
+
+localStorage.setItem('theme', 'dark');
+
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+}
